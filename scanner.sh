d55e8bce8515adbcfd4ff372b946a456088e3eb5
alien-keric <alienkeric@gmail.com>
2024-04-06 15:31:43 -0400

    Another commit for today

--- scanner.sh
+++ scanner.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
 
+##checking the package is installed or not
+
+package1 = "rustscan"
+package2 = "nmap"
+package3 = "whois"
+
+if  dpkg -l | grep -q "^ii.*$package1"; then
+	echo "$package1 is already installed"
+else 
+	sudo apt install $package1
+
+fi
+
+if dpkg -l | grep -q "^ii.*$package2"; then
+	echo "$package2 is already installed"
+
+else 
+	sudo apt install $package2
+
+fi
+
 ## simple scanner for pentesting
-sudo apt install  rustscan
+
+echo "Enter the target ip address"
+read target
+
+
+echo "Oky lets have some fun by scanning our target $target"
+
 
