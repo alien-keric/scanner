40afd823134c27179cb26f4b618199f2e29f1c4b
alien-keric <alienkeric@gmail.com>
2024-04-06 15:46:50 -0400

    Almost there

--- scanner.sh
+++ scanner.sh
@@ -2,39 +2,32 @@
 
 ##checking the package is installed or not
 
-package1 = "rustscan"
-package2 = "nmap"
-package3 = "whois"
+package1="rustscan"
+package2="nmap"
+package3="whois"
 
-if  dpkg -l | grep -q "^ii.*$package1"; then
-	echo "$package1 is already installed"
+if dpkg -l | grep -q "^ii.*$package1"; then
+    echo "$package1 is already installed"
 else 
-	sudo apt install $package1
-
+    sudo apt install $package1
 fi
 
 if dpkg -l | grep -q "^ii.*$package2"; then
-	echo "$package2 is already installed"
-
+    echo "$package2 is already installed"
 else 
-	sudo apt install $package2
-
+    sudo apt install $package2
 fi
 
 if dpkg -l | grep -q "^ii.*$package3"; then
-	echo "$package3 is already installed"
-
+    echo "$package3 is already installed"
 else
-	sudo apt install $package3
-
-
+    sudo apt install $package3
+fi
 
 ## simple scanner for pentesting
 
 echo "Enter the target ip address"
 read target
 
-
-echo "Oky lets have some fun by scanning our target $target"
-
+echo "Okay let's have some fun by scanning our target $target"
 
